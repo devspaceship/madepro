36d1fc7b77f6e27863eae55f871ea1b23c4ee83f
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-01-06 09:51:44 +0100

    [testing] moved unittests from integration

--- src/solvers/dp.rs
+++ src/solvers/dp.rs
@@ -113,3 +113,50 @@ where
     );
     policy_value_iteration(mdp, config)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::environments::gridworld::{
+        assert_policy_optimal, assert_state_value_correct, get_optimal_policy, get_test_config,
+        get_test_mdp, get_test_state_value,
+    };
+
+    #[test]
+    fn test_policy_evaluation() {
+        let mdp = get_test_mdp();
+        let config = get_test_config();
+        let policy = get_optimal_policy(mdp.get_states(), mdp.get_actions());
+        let state_value = policy_evaluation(&mdp, &config, &policy, None);
+        assert_state_value_correct(&state_value);
+    }
+
+    #[test]
+    fn test_policy_inference() {
+        let mdp = get_test_mdp();
+        let config = get_test_config();
+        let state_value = get_test_state_value(mdp.get_states());
+        let inferred_policy = policy_improvement(&mdp, &config, &state_value);
+        assert_policy_optimal(&inferred_policy);
+    }
+
+    #[test]
+    fn test_policy_iteration() {
+        let mdp = get_test_mdp();
+        let config = get_test_config();
+        let state_value = policy_iteration(&mdp, &config);
+        let policy = policy_improvement(&mdp, &config, &state_value);
+        assert_state_value_correct(&state_value);
+        assert_policy_optimal(&policy);
+    }
+
+    #[test]
+    fn test_value_iteration() {
+        let mdp = get_test_mdp();
+        let config = get_test_config().iterations_before_improvement(Some(3));
+        let state_value = value_iteration(&mdp, &config);
+        let policy = policy_improvement(&mdp, &config, &state_value);
+        assert_state_value_correct(&state_value);
+        assert_policy_optimal(&policy);
+    }
+}
